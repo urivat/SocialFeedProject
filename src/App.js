88ef6013c03ac0b0c15f67ab096b975c4deb4909
rashad porter <rashad.porter18@gmail.com>
2022-04-04 16:57:23 -0400

    added a table for name and post entries and instantiated components in the app component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
-
-
+import Post from './Components/post/Post';
+import DisplayPosts from './Components/DisplayPosts/DisplayPosts';
 
 function App() {
+  const [entries, setEntries] = useState([
+    {name:'rashad', post:'N/A'},
+  ])
+  
+function addNewPost(entry){
+  let tempentries = [entries, entry];
+
+  setEntries(tempentries);
+}
+  
+  
   return (
-    <div className="App">
-      Hello World
+    <div>
+      <Post appEntries={entries}/>
     </div>
   );
 }
